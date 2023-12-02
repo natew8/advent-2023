240c0bfb6e406308d6e6292de6fb453892f1a620
Nate Waite <nate.waite@spiff.com>
2023-12-01 19:28:29 -0700

    day one complete

--- one/part_1.exs
+++ one/part_1.exs
@@ -1,12 +1,11 @@
 defmodule One do
   def get_numbers(input) do
     input
-    |> Enum.map(&Regex.scan(~r/\d/, &1) |> Enum.flat_map(fn l -> l end))
-    |> Enum.map(fn nums ->
+    |> Enum.map(fn s ->
+      nums = Regex.scan(~r/\d/, s) |> List.flatten()
       Enum.join([hd(nums), List.last(nums)]) |> String.to_integer()
     end)
   end
-
 end
 
 File.read!(Path.join(__DIR__, "input"))
